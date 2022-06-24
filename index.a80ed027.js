452c9068c77b76ed7fa97209ec8164bafcdc25d3
oleksiiPut <94678751+oleksiiPut@users.noreply.github.com>
2022-06-24 16:43:02 +0000

    Deploying to gh-pages from @ oleksiiPut/goit-js-hw-11@4a2ce873a0fdcec61a75e125659e3ebf31cb5b21 🚀

---
+++ index.a80ed027.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",searchImage);
+//# sourceMappingURL=index.a80ed027.js.map
---
+++ index.b160d8b7.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",searchImage);
+//# sourceMappingURL=index.b160d8b7.js.map
